09aca1f7a0b78595269bbcd03dc114aa1cf42cc9
AniaaP <109247164+AniaaP@users.noreply.github.com>
2022-08-11 13:54:08 +0200

    task 3: first test 2

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,15 +6,15 @@ class LoginPage(BasePage):
     password_field_xpath = "//*[@id='password']"
     remaind_password_hyperlink_xpath = "//*[@id='__next']/form/div/div[1]/a"
     language_comobox_xpath = "//*[@id='__next']/form/div/div[2]/div/div"
-      sign_in_button_xpath = "//*[@id='__next']/form/div/div[2]/button/span[1]"
+    sign_in_button_xpath = "//*[@id='__next']/form/div/div[2]/button/span[1]"
 
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
 
 
-def type_in_password(self, email):
-    self.field_send_keys(self.login_field_xpath, email)
+    def type_in_password(self, email):
+        self.field_send_keys(self.login_field_xpath, email)
 
     def click_on_the_sign_in_button(self, email):
         self.field_send_keys(self.login_field_xpath, email)
